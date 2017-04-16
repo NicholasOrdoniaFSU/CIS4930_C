9618e35fd93b3197b4fca49566aed876d7aed076
NicholasOrdoniaFSU <no12b@my.fsu.edu>
2017-04-16 16:07:51 -0400

    Add files via upload

--- flask_site/webapp/main.py
+++ flask_site/webapp/main.py
@@ -1,31 +1,29 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect, url_for
+import os
+from werkzeug.utils import secure_filename
+from forms import ModifyImage
 
 app = Flask(__name__)
-
+app.config.from_object('config')
+'''
 @app.route('/') # '/' is the root homepage
 def index():
-	return render_template("main.html")
-
+	return render_template("base.html",title='Image It', form=form, info=info)
 '''
-@app.route('/imageit', methods=['GET','POST']) # '/' is the root homepage
-def imageit():
-	if request.method == 'POST':
-		return "You are using POST"
-	elif request.method == 'GET':
-		return "you are using GET"			
+@app.route('/', methods=['GET','POST'])
+def search():
+	info = None
+	form = ModifyImage()
+	if form.validate_on_submit():
+		image_loc = form.image_loc.data
+		info - get_info(image_loc)
+	return render_template('base.html',title='Image It', form=form, info=info)
+'''
+UPLOAD_FOLDER = '/path/to/the/uploads'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
-
-@app.route('/imageit')
-def imageit():
-	return "<h2>This is the imageit start</h2>"
-
-@app.route('/team/<teammember>')
-def team(teammember):
-	return "<h2>Hello, my name is %s</h2>" % teammember
-
-@app.route('/team/<int:memberid>')
-def teammember(memberid):
-	return "<h2>This members id is %s</h2>" % memberid
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 '''
 
 if __name__ == "__main__":
