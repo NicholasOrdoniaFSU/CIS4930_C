70156de14bfcab9971dc0b9dada267d337879bdb
nak13c <nak13c@my.fsu.edu>
2017-04-23 14:50:39 -0400

    Add files via upload

--- flask_site/webapp/main.py
+++ flask_site/webapp/main.py
@@ -13,7 +13,7 @@ def search():
 	info = []
 	#return request.form['options']
 	#return render_template('base.html',title='Image It', form=form, info=len(form.data))
-	if form.data['image_loc'] and request.form['options']: #check input field and radio button
+	if form.data['image_loc'] and ("grey" == request.form['options']): #check input field and radio button
 		imgfile = form.data['image_loc'].split('/')[-1]
 		info.append(request.form['options']) #get filter type in list
 		info.append(imgfile) #add the image loc to list
@@ -28,10 +28,142 @@ def search():
 
 		newPic = Image.new(pic.mode, pic.size)
 		newPic.putdata(gr)
+		newPic.save("static/grey.jpg")
+		info.append(form.data)
+		info.append(form.data['image_loc'])
+		return render_template('base.html',title='Image It', form=form, info=info)
+	elif form.data['image_loc'] and ("red" == request.form['options']): #check input field and radio button
+		imgfile = form.data['image_loc'].split('/')[-1]
+		info.append(request.form['options']) #get filter type in list
+		info.append(imgfile) #add the image loc to list
+		pic = Image.open(form.data['image_loc'])
+		pixels = list(pic.getdata())
+		gr = []
+
+		for r,g,b in pixels:
+			f = (r*10,g,b)
+			gr.append(f)
+
+		newPic = Image.new(pic.mode, pic.size)
+		newPic.putdata(gr)
+		newPic.save("static/red.jpg")
+		info.append(form.data)
+		info.append(form.data['image_loc'])
+		return render_template('base.html',title='Image It', form=form, info=info)
+	elif form.data['image_loc'] and ("green" == request.form['options']): #check input field and radio button
+		imgfile = form.data['image_loc'].split('/')[-1]
+		info.append(request.form['options']) #get filter type in list
+		info.append(imgfile) #add the image loc to list
+		pic = Image.open(form.data['image_loc'])
+		pixels = list(pic.getdata())
+		gr = []
+
+		for r,g,b in pixels:
+			f = (r,g*10,b)
+			gr.append(f)
+
+		newPic = Image.new(pic.mode, pic.size)
+		newPic.putdata(gr)
+		newPic.save("static/green.jpg")
+		info.append(form.data)
+		info.append(form.data['image_loc'])
+		return render_template('base.html',title='Image It', form=form, info=info)
+	elif form.data['image_loc'] and ("blue" == request.form['options']): #check input field and radio button
+		imgfile = form.data['image_loc'].split('/')[-1]
+		info.append(request.form['options']) #get filter type in list
+		info.append(imgfile) #add the image loc to list
+		pic = Image.open(form.data['image_loc'])
+		pixels = list(pic.getdata())
+		gr = []
+
+		for r,g,b in pixels:
+			f = (r,g,b*10)
+			gr.append(f)
+
+		newPic = Image.new(pic.mode, pic.size)
+		newPic.putdata(gr)
+		newPic.save("static/blue.jpg")
+		info.append(form.data)
+		info.append(form.data['image_loc'])
+		return render_template('base.html',title='Image It', form=form, info=info)
+	elif form.data['image_loc'] and ("bw" == request.form['options']): #check input field and radio button
+		imgfile = form.data['image_loc'].split('/')[-1]
+		info.append(request.form['options']) #get filter type in list
+		info.append(imgfile) #add the image loc to list
+		pic = Image.open(form.data['image_loc'])
+		pixels = list(pic.getdata())
+		gr = []
+
+		for r,g,b in pixels:
+			avg = (r+g+b)/3
+			if avg >= 128:
+				avg = 255
+			else:
+				avg = 0
+			f = (avg,avg,avg)
+			gr.append(f)
+
+		newPic = Image.new(pic.mode, pic.size)
+		newPic.putdata(gr)
 		newPic.save("static/bw.jpg")
 		info.append(form.data)
 		info.append(form.data['image_loc'])
 		return render_template('base.html',title='Image It', form=form, info=info)
+	elif form.data['image_loc'] and ("pink" == request.form['options']): #check input field and radio button
+		imgfile = form.data['image_loc'].split('/')[-1]
+		info.append(request.form['options']) #get filter type in list
+		info.append(imgfile) #add the image loc to list
+		pic = Image.open(form.data['image_loc'])
+		pixels = list(pic.getdata())
+		gr = []
+
+		for r,g,b in pixels:
+			f = (r*2,g,b*2)
+			gr.append(f)
+
+		newPic = Image.new(pic.mode, pic.size)
+		newPic.putdata(gr)
+		newPic.save("static/pink.jpg")
+		info.append(form.data)
+		info.append(form.data['image_loc'])
+		return render_template('base.html',title='Image It', form=form, info=info)
+	elif form.data['image_loc'] and ("bt" == request.form['options']): #check input field and radio button
+		imgfile = form.data['image_loc'].split('/')[-1]
+		info.append(request.form['options']) #get filter type in list
+		info.append(imgfile) #add the image loc to list
+		pic = Image.open(form.data['image_loc'])
+		pixels = list(pic.getdata())
+		gr = []
+
+		for r,g,b in pixels:
+			f = (int(round(r*.5)),int(round(g*0.7777)),b)
+			gr.append(f)
+
+		newPic = Image.new(pic.mode, pic.size)
+		newPic.putdata(gr)
+		newPic.save("static/BedTime.jpg")
+		info.append(form.data)
+		info.append(form.data['image_loc'])
+		return render_template('base.html',title='Image It', form=form, info=info)
+	elif form.data['image_loc'] and ("energy" == request.form['options']): #check input field and radio button
+		imgfile = form.data['image_loc'].split('/')[-1]
+		info.append(request.form['options']) #get filter type in list
+		info.append(imgfile) #add the image loc to list
+		pic = Image.open(form.data['image_loc'])
+		pixels = list(pic.getdata())
+		gr = []
+
+		for r,g,b in pixels:
+			f = (int(round(r*2)),int(round(g*0.63)),int(round(b*0.2)))
+			gr.append(f)
+
+		newPic = Image.new(pic.mode, pic.size)
+		newPic.putdata(gr)
+		newPic.save("static/energy.jpg")
+		info.append(form.data)
+		info.append(form.data['image_loc'])
+		return render_template('base.html',title='Image It', form=form, info=info)
+	
 	else:
 		return render_template('base.html',title='Image It', form=form, info=info)
 	'''
