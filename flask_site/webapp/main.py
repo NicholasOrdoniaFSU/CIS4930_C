3a89dee5d3bb4aebd9b9b4b664eb6cc8bde10c68
NicholasOrdoniaFSU <no12b@my.fsu.edu>
2017-04-23 13:08:34 -0400

    Update main.py

--- flask_site/webapp/main.py
+++ flask_site/webapp/main.py
@@ -10,7 +10,31 @@ app.config.from_object('config')
 @app.route('/', methods=['GET','POST'])
 def search():
 	form = ModifyImage()
+	info = []
+	#return request.form['options']
 	#return render_template('base.html',title='Image It', form=form, info=len(form.data))
+	if form.data['image_loc'] and request.form['options']: #check input field and radio button
+		imgfile = form.data['image_loc'].split('/')[-1]
+		info.append(request.form['options']) #get filter type in list
+		info.append(imgfile) #add the image loc to list
+		pic = Image.open(form.data['image_loc'])
+		pixels = list(pic.getdata())
+		gr = []
+
+		for r,g,b in pixels:
+			avg = int((r+g+b)/3)
+			f = (avg,avg,avg)
+			gr.append(f)
+
+		newPic = Image.new(pic.mode, pic.size)
+		newPic.putdata(gr)
+		newPic.save("static/bw.jpg")
+		info.append(form.data)
+		info.append(form.data['image_loc'])
+		return render_template('base.html',title='Image It', form=form, info=info)
+	else:
+		return render_template('base.html',title='Image It', form=form, info=info)
+	'''
 	if form.data['image_loc_bw'] is not None and len(form.data['image_loc_bw']) > 4:
 		pic = Image.open(form.data['image_loc_bw'])
 		pixels = list(pic.getdata())
@@ -55,9 +79,8 @@ def search():
 		newPic.putdata(gr)
 		newPic.save("static/blue.jpg")
 		return render_template('base.html',title='Image It', form=form, info=newPic)
-
-	else:
-		return render_template('base.html',title='Image It', form=form, info="None")
+	'''
+	
 
 if __name__ == "__main__":
 	app.run(debug=True)
