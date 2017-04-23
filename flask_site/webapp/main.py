dfbb9a96bd60013136517c9541731c5658f62189
nak13c <nak13c@my.fsu.edu>
2017-04-23 15:27:59 -0400

    Add files via upload

--- flask_site/webapp/main.py
+++ flask_site/webapp/main.py
@@ -163,6 +163,90 @@ def search():
 		info.append(form.data)
 		info.append(form.data['image_loc'])
 		return render_template('base.html',title='Image It', form=form, info=info)
+	elif form.data['image_loc'] and ("blur" == request.form['options']): #check input field and radio button
+		imgfile = form.data['image_loc'].split('/')[-1]
+		info.append(request.form['options']) #get filter type in list
+		info.append(imgfile) #add the image loc to list
+		pic = Image.open(form.data['image_loc'])
+
+		newPic = pic.filter(ImageFilter.BLUR)
+
+		newPic.save("static/blur.jpg")
+		info.append(form.data)
+		info.append(form.data['image_loc'])
+		return render_template('base.html',title='Image It', form=form, info=info)
+	elif form.data['image_loc'] and ("con" == request.form['options']): #check input field and radio button
+		imgfile = form.data['image_loc'].split('/')[-1]
+		info.append(request.form['options']) #get filter type in list
+		info.append(imgfile) #add the image loc to list
+		pic = Image.open(form.data['image_loc'])
+
+		newPic = pic.filter(ImageFilter.CONTOUR)
+
+		newPic.save("static/con.jpg")
+		info.append(form.data)
+		info.append(form.data['image_loc'])
+		return render_template('base.html',title='Image It', form=form, info=info)
+	elif form.data['image_loc'] and ("det" == request.form['options']): #check input field and radio button
+		imgfile = form.data['image_loc'].split('/')[-1]
+		info.append(request.form['options']) #get filter type in list
+		info.append(imgfile) #add the image loc to list
+		pic = Image.open(form.data['image_loc'])
+
+		newPic = pic.filter(ImageFilter.DETAIL)
+
+		newPic.save("static/detail.jpg")
+		info.append(form.data)
+		info.append(form.data['image_loc'])
+		return render_template('base.html',title='Image It', form=form, info=info)
+	elif form.data['image_loc'] and ("edge" == request.form['options']): #check input field and radio button
+		imgfile = form.data['image_loc'].split('/')[-1]
+		info.append(request.form['options']) #get filter type in list
+		info.append(imgfile) #add the image loc to list
+		pic = Image.open(form.data['image_loc'])
+
+		newPic = pic.filter(ImageFilter.EDGE_ENHANCE)
+
+		newPic.save("static/edgeE.jpg")
+		info.append(form.data)
+		info.append(form.data['image_loc'])
+		return render_template('base.html',title='Image It', form=form, info=info)
+	elif form.data['image_loc'] and ("emb" == request.form['options']): #check input field and radio button
+		imgfile = form.data['image_loc'].split('/')[-1]
+		info.append(request.form['options']) #get filter type in list
+		info.append(imgfile) #add the image loc to list
+		pic = Image.open(form.data['image_loc'])
+
+		newPic = pic.filter(ImageFilter.EMBOSS)
+
+		newPic.save("static/emb.jpg")
+		info.append(form.data)
+		info.append(form.data['image_loc'])
+		return render_template('base.html',title='Image It', form=form, info=info)
+	elif form.data['image_loc'] and ("smo" == request.form['options']): #check input field and radio button
+		imgfile = form.data['image_loc'].split('/')[-1]
+		info.append(request.form['options']) #get filter type in list
+		info.append(imgfile) #add the image loc to list
+		pic = Image.open(form.data['image_loc'])
+
+		newPic = pic.filter(ImageFilter.SMOOTH)
+
+		newPic.save("static/smooth.jpg")
+		info.append(form.data)
+		info.append(form.data['image_loc'])
+		return render_template('base.html',title='Image It', form=form, info=info)
+	elif form.data['image_loc'] and ("shar" == request.form['options']): #check input field and radio button
+		imgfile = form.data['image_loc'].split('/')[-1]
+		info.append(request.form['options']) #get filter type in list
+		info.append(imgfile) #add the image loc to list
+		pic = Image.open(form.data['image_loc'])
+
+		newPic = pic.filter(ImageFilter.SHARPEN)
+
+		newPic.save("static/sharpen.jpg")
+		info.append(form.data)
+		info.append(form.data['image_loc'])
+		return render_template('base.html',title='Image It', form=form, info=info)
 	
 	else:
 		return render_template('base.html',title='Image It', form=form, info=info)
